e85c5a28ccc90b098d6fae780661e6150f11bc95
AWildDevAppears <joshuacburgess32@gmail.com>
2024-05-26 14:06:45 +0100

    Add cards

--- offspringengine/services/database.py
+++ offspringengine/services/database.py
@@ -1,3 +1,4 @@
+from offspringengine.models.card import Card, CardModifierIndex
 from offspringengine.models.equipment import Equipment
 from tinydb import TinyDB
 from tinydb.queries import Query
@@ -7,8 +8,9 @@ db = TinyDB("./database.json")
 items_table = db.table("ITEMS", cache_size=30)
 item_mods_table = db.table("ITEM_MODIFIERS")
 maps_table = db.table("MAPS", cache_size=30)
+card_table = db.table("CARDS")
 
-
+# - Items ------
 def get_item_by_id(id: str) -> Equipment:
     item: Equipment = Query()
     return items_table.search(item.id == id)[0]
@@ -23,4 +25,38 @@ item_mod_count = item_mods_table.count(Query().id != "")
 def get_item_mod_count() -> int:
     return item_mod_count
 
+# - Cards ------
+def to_card(doc: Document):
+    mods_raw: list[Document] = doc["modifiers"]
+    mods_real: list[CardModifierIndex] = list()
+
+    for mod in mods_raw:
+        mods_real.append(CardModifierIndex(mod["id"], mod["turns"], mod["amount"], mod["mode"]))
+
+    return Card(doc["name"], doc["description"], doc["damage"], doc["target"], modsReal)
+
+def get_card_by_name(name: str):
+    card = Query()
+    doc = card_table.search(card.name == name)[0]
+    return to_card(doc)
+
+def get_card_by_id(idx: int) -> Card:
+    doc = card_table.get(doc_id=idx)
+    return to_card(doc)
+
+card_count = card_table.count(Query().id != "")
+def get_card_count() -> int:
+    if not card_count:
+        return 0
+
+    return card_count
+
+def get_all_cards():
+    cards_raw = card_table.all()
+    cards_real: list[Card] = list()
+
+    for card in cards_raw:
+        cards_real.append(to_card(card))
+
+    return cards_real
 
